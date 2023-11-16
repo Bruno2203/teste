1f03201f2231ea3cc1c9243f3464b4f963dba963
Bruno2203 <125492134+Bruno2203@users.noreply.github.com>
2023-11-16 08:29:02 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,15 @@
+function potencia(){
+    let n = 5;
+    let fat = 5;
+    let soma = 0;
+    let potencia = 0;
+    let i = 5;
+
+    while(n>=1){
+        potencia = Math.pow(n,2);
+        n=n-1;
+        while(n>=1){
+            soma = fat*(fat-1);
+        }
+    }
+}
